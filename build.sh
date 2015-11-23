93d2f34680b22e3a78829ac9ced87031835933a0
root <fabio.verboso@gmail.com>
2015-11-23 15:20:29 +0100

    skip http restarting on building

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-python setup.py build; python setup.py install; systemctl restart httpd;
+python setup.py build; python setup.py install; 
+systemctl restart httpd;
 rm -rf build
 rm -rf iotronic.egg-info 
 rm -rf dist
